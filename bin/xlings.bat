009be0c7179e6a8c5953d029a80106b1c4cc03ef
sunrisepeak <speakshen@163.com>
2024-11-04 17:07:43 +0800

    core: bin: fix windows xlings.bat's path issue
    fix cd command failed for from c to d
    add '/d' arg support switch
    
    Signed-off-by: sunrisepeak <speakshen@163.com>

--- bin/xlings.bat
+++ bin/xlings.bat
@@ -10,26 +10,26 @@ set arg2=%2
 set "commands=help -h uninstall update drepo config"
 
 if "%arg1%"=="" (
-    cd %XLINGS_DIR%/core
+    cd /d %XLINGS_DIR%/core
     xmake xlings %XLINGS_RUN_DIR% %arg1% %arg2%
-    cd %XLINGS_RUN_DIR%
+    cd /d %XLINGS_RUN_DIR%
     exit /b
 )
 
 if "%arg1%"=="run" (
     if not exist "%XLINGS_CACHE_DIR%" mkdir "%XLINGS_CACHE_DIR%"
-    cd %XLINGS_DIR%/core
+    cd /d %XLINGS_DIR%/core
     xmake xlings %XLINGS_RUN_DIR% run %arg2%
-    cd %XLINGS_RUN_DIR%
+    cd /d %XLINGS_RUN_DIR%
     exit /b
 )
 
 echo %commands% | findstr /r "\<%arg1%\>" >nul
 if not errorlevel 1 (
-    cd %XLINGS_DIR%/core
+    cd /d %XLINGS_DIR%/core
     xmake xlings %XLINGS_RUN_DIR% %arg1% %arg2%
 ) else if not exist "%XLINGS_RUN_DIR%/config.xlings" (
-    cd %XLINGS_DIR%/core
+    cd /d %XLINGS_DIR%/core
     if "%arg1%"=="install" (
         xmake xlings %XLINGS_RUN_DIR% install %arg2%
     ) else (
@@ -41,9 +41,9 @@ if not errorlevel 1 (
     copy /y "%XLINGS_RUN_DIR%\config.xlings" "%XLINGS_CACHE_DIR%\config.xlings.lua"
     copy /y "%XLINGS_DIR%\tools\template.win.xlings" "%XLINGS_CACHE_DIR%\xmake.lua"
 
-    cd %XLINGS_CACHE_DIR%
+    cd /d %XLINGS_CACHE_DIR%
 
     xmake xlings %XLINGS_RUN_DIR% %arg1% %arg2%
 )
 
-cd %XLINGS_RUN_DIR%
+cd /d %XLINGS_RUN_DIR%
