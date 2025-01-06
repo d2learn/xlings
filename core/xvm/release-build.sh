50d997eba6d028e3d8f1109e4e794ece11f7a635
sunrisepeak <speakshen@163.com>
2025-01-06 21:22:25 +0800

    update

--- core/xvm/release-build.sh
+++ core/xvm/release-build.sh
@@ -1,7 +1,9 @@
-LINUX_RELEASE_DIR=target/release
-WINDOWS_RELEASE_DIR=target/x86_64-pc-windows-gnu/release
+LINUX_RELEASE_DIR=`pwd`/target/release
+WINDOWS_RELEASE_DIR=`pwd`/target/x86_64-pc-windows-gnu/release
 
 cargo build --release
-tar -czf $LINUX_RELEASE_DIR/xvm-dev-x86_64-linux-gnu.tar.gz $LINUX_RELEASE_DIR/xvm
 cargo build --target x86_64-pc-windows-gnu --release
-zip -r $WINDOWS_RELEASE_DIR/xvm-dev-x86_64-pc-windows-gnu.zip $WINDOWS_RELEASE_DIR/xvm.exe
+cd $LINUX_RELEASE_DIR
+tar -czf xvm-dev-x86_64-linux-gnu.tar.gz xvm
+cd $WINDOWS_RELEASE_DIR
+zip -r xvm-dev-x86_64-pc-windows-gnu.zip xvm.exe
--- tools/install.unix.sh
+++ tools/install.unix.sh
@@ -44,6 +44,9 @@ fi
 cd $RUN_DIR/core
 xmake xlings unused install xlings
 
+## install xvm
+xim -i xvm -y
+
 # 3. install info
 echo -e "${GREEN}[xlings]: xlings installed${RESET}"
 
