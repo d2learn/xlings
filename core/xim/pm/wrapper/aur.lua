fa7c9b09437673ff6dd4baf3d6d9277cf71f2101
sunrisepeak <speakshen@163.com>
2025-01-10 15:44:22 +0800

    update aur

--- core/xim/pm/wrapper/aur.lua
+++ core/xim/pm/wrapper/aur.lua
@@ -1,76 +1,83 @@
+import("xim.base.runtime")
 import("xim.pm.wrapper.pacman")
 
-function installed(git_url)
-    return pacman.installed(parse_name_from_git_url(git_url))
+local aur_pkgs_dir = path.join(runtime.get_xim_data_dir(), "aur_pkgs")
+local aur_url_template = "https://aur.archlinux.org/%s.git"
+local aur_pkgs_info = {}
+
+function installed(name)
+    return pacman.installed(name)
 end
 
-function deps(git_url)
-    return pacman.deps(parse_name_from_git_url(git_url))
+function deps(name)
+    local info = aur_info(name)
+    -- table to string
+    return info["Depends"]
 end
 
-function install(git_url)
-    local name = parse_name_from_git_url(git_url)
+function install(name)
+    os.cd(aur_pkgs_dir)
 
     -- 克隆 AUR 仓库
-    if not clone_aur_package(name, git_url) then
-        cprint(string.format("Failed to clone AUR repository: '%s'", git_url))
-    end
-
-    -- 进入包目录
-    os.tryrm(name)
-    local ok = os.cd(name)
-    if not ok then
-        cprint(string.format("Failed to enter directory: '%s'", name))
+    if not os.isdir(name) then
+        local git_url = to_git_url(name)
+        os.run("git clone %s", git_url)
+        os.cd(name)
+    else
+        cpint("%s already exists, try to update...", name)
+        os.cd(name)
+        os.run("git pull")
     end
 
     -- 构建并安装包
-    local installed_ok = build_and_install(name)
-    if not installed_ok then
-        cprint(string.format("Failed to build and install package: '%s'", name))
-    end
+    os.run("makepkg -si")
 
     return true
 end
 
-function uninstall(git_url)
-    return pacman.uninstall(parse_name_from_git_url(git_url))
+function uninstall(name)
+    return pacman.uninstall(name)
 end
 
-function info(git_url)
-    return pacman.info(parse_name_from_git_url(git_url))
+function info(name)
+    local info = aur_info(name)
+    return format([[
+\t[ XVM-AUR Package Info]
+
+Name: %s
+Version: %s
+License: %s
+Maintainer: %s
+URL: %s
+Popularity: %s
+
+\t%s
+
+        ]], info["Name"], info["Version"], info["License"], info["Maintainer"],
+        info["URL"], info["Popularity"], info["Description"]
+    )
 end
 
-function parse_name_from_git_url(git_url)
-    -- 假设输入形如 "https://aur.archlinux.org/<name>.git"
-    local name = git_url:match("^https://aur%.archlinux%.org/(.-)%.git$")
-    if not name then
-        cprint(string.format("Failed to parse package name from git URL: '%s'", git_url))
+function aur_info(name)
+    -- check cache
+    if aur_pkgs_info["results"] == nil or aur_pkgs_info["results"]["PackageBase"] ~= name then
+        -- https://github.com/d2learn/xlings/pull/67#issuecomment-2580867360
+        local curl_cmd = format([[curl -X 'GET' 'https://aur.archlinux.org/rpc/v5/info/%s' -H 'accept: application/json']], name)
+        local ok, out = os.iorun(curl_cmd)
+        if ok then
+            aur_pkgs_info = json.decode(out)
+        else
+            cprint("Failed to get AUR package info: %s", out)
+        end
     end
-    return name
+    return aur_pkgs_info["results"]
 end
 
-function clone_aur_package(name, git_url)
-    local ok = os.execv("git", {"clone", git_url, name})
-    return ok == 0
-end
-
-function build_and_install(name)
-    local ok = os.execv("makepkg", {"-si"})
-    return ok == 0
+function to_git_url(name)
+    -- 假设输入形如 "https://aur.archlinux.org/<name>.git"
+    return string.format(aur_url_template, name)
 end
 
 function main()
     local aur_git_url = "https://aur.archlinux.org/yay.git"
-
-    -- 检查是否已安装
-    cprint(installed(aur_git_url)) -- true or false
-
-    -- 获取依赖
-    cprint(deps(aur_git_url))
-
-    -- 安装 AUR 包
-    aur_install(aur_git_url)
-
-    -- 获取包信息
-    cprint(info(aur_git_url))
 end
