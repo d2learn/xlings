171d645b6639dbbc521542cc97a0dd8773cef6a3
sunrisepeak <speakshen@163.com>
2025-05-31 22:10:23 +0800

    update

--- tools/install.unix.sh
+++ tools/install.unix.sh
@@ -3,12 +3,12 @@
 RUN_DIR=`pwd`
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
-XMAKE_BIN="$ROOT_DIR/bin/xmake"
+XMAKE_BIN="xmake"
 
 XLINGS_HOME="/home/xlings"
 XLINGS_SYMLINK="/usr/bin/xlings"
 
-if [ "$(uname)" = "Darwin" ]; then
+if [ "$(uname)" == "Darwin" ]; then
     XLINGS_HOME="/Users/xlings"
     XLINGS_SYMLINK="/usr/local/bin/xlings"
 fi
@@ -28,7 +28,8 @@ echo -e "${PURPLE}[xlings]: start detect environment and try to auto config...${
 if ! command -v xmake &> /dev/null
 then
     echo -e "${PURPLE}[xlings]: start install xmake...${RESET}"
-    curl -L https://github.com/xlings-res/xmake/releases/download/2.9.9/xmake -o $XMAKE_BIN
+    XMAKE_BIN="$ROOT_DIR/bin/xmake"
+    curl -sSL https://github.com/xlings-res/xmake/releases/download/2.9.9/xmake -o $XMAKE_BIN
     chmod +x $XMAKE_BIN
 else
     echo -e "${GREEN}[xlings]: xmake installed${RESET}"
@@ -46,7 +47,7 @@ fi
 
 # 2. install xlings
 cd $ROOT_DIR/core
-xmake xlings unused self enforce-install
+$XMAKE_BIN xlings unused self enforce-install
 sudo ln -sf $XLINGS_HOME/.xlings_data/bin/xlings $XLINGS_SYMLINK
 
 export PATH="$XLINGS_HOME/.xlings_data/bin:$PATH"
