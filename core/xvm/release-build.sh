15c8c53859768c0dcdd73f88866fd2c72891f7a6
sunrisepeak <speakshen@163.com>
2025-06-09 12:59:26 +0800

    xvm: use musl to avoid glibc issue

--- core/xvm/release-build.sh
+++ core/xvm/release-build.sh
@@ -2,18 +2,18 @@
 
 CURRENT_DATE=$(date +"%Y%m%d%H%M%S")
 
-LINUX_RELEASE_DIR=$(pwd)/target/release
+LINUX_RELEASE_DIR=$(pwd)/target/x86_64-unknown-linux-musl/release
 WINDOWS_RELEASE_DIR=$(pwd)/target/x86_64-pc-windows-gnu/release
 
-cargo build --release
+cargo build --target x86_64-unknown-linux-musl --release
 cargo build --target x86_64-pc-windows-gnu --release
 
 cd "$LINUX_RELEASE_DIR"
-LINUX_ARCHIVE_NAME="xvm-dev-x86_64-linux-gnu-${CURRENT_DATE}.tar.gz"
+LINUX_ARCHIVE_NAME="xvm-0.0.2-linux-x86_64-${CURRENT_DATE}.tar.gz"
 tar -czf "$LINUX_ARCHIVE_NAME" xvm xvm-shim
 echo "✅ Linux archive created: $LINUX_ARCHIVE_NAME"
 
 cd "$WINDOWS_RELEASE_DIR"
-WINDOWS_ARCHIVE_NAME="xvm-dev-x86_64-pc-windows-gnu-${CURRENT_DATE}.zip"
+WINDOWS_ARCHIVE_NAME="xvm-0.0.2-x86_64-pc-windows-${CURRENT_DATE}.zip"
 zip -r "$WINDOWS_ARCHIVE_NAME" xvm.exe xvm-shim.exe
 echo "✅ Windows archive created: $WINDOWS_ARCHIVE_NAME"
