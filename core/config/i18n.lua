65e59503f790ca462187c57e1031bc1536e08759
sunrisepeak <speakshen@163.com>
2025-06-23 19:26:37 +0800

    core: bugfix: fix wmic issues for win11+
    Signed-off-by: sunrisepeak <speakshen@163.com>

--- core/config/i18n.lua
+++ core/config/i18n.lua
@@ -1,4 +1,5 @@
 import("core.base.json")
+import("lib.detect.find_tool")
 
 local __data = nil
 
@@ -6,13 +7,20 @@ function data()
     --local config = platform.get_config_info()
     local local_lang = "en" -- Default language
 
+    -- TODO: config language by xlings.json
     if is_host("linux") then
         local tmp_local_lang = os.getenv("LANG") or "en"
         if tmp_local_lang:find("zh") then
             local_lang = "zh"
         end
     elseif is_host("windows") then
-        local tmp_local_lang = os.iorun([[wmic os get locale]])
+        local tmp_local_lang = nil
+        if find_tool("wmic") then -- Windows 10 and earlier
+            -- wmic is deprecated in Windows 11, but still available
+            tmp_local_lang = os.iorun([[wmic os get locale]])
+        else -- win11+
+            tmp_local_lang = os.iorun([[powershell -NoProfile -ExecutionPolicy Bypass -Command "'{0:X4}' -f (Get-Culture).LCID"]])
+        end
         if tmp_local_lang and tmp_local_lang:find("0804") then
             local_lang = "zh"
         end
