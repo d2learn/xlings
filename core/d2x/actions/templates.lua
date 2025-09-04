11af7c9365b993d61c0a379d75314bd57c7fec32
sunrisepeak <speakshen@163.com>
2025-09-04 20:52:39 +0800

    core: optimize 'xlings new' and add support for xpkg's template type

--- core/d2x/actions/templates.lua
+++ core/d2x/actions/templates.lua
@@ -1,5 +1,48 @@
 import ("platform")
 
+-- TODO: xvm?
+import("xim.base.xvm")
+
+-- for xpkg-template
+function get_template_dir(template)
+
+    -- namespace:pkgname@version
+    local namespace
+    local pkgname
+    local version
+
+    local parts = template:split("@")
+    if #parts == 2 then
+        version = parts[2]
+    end
+    parts = parts[1]:split(":")
+    if #parts == 2 then
+        namespace = parts[1]
+        -- TODO: template xvm name? namespace-pkgname?
+        pkgname = namespace .. "-" .. parts[2]
+    else
+        pkgname = parts[1]
+    end
+
+    if not xvm.has(pkgname) then
+        cprint("[xlings]: template [ ${yellow}%s${clear} ] not found, try to install it...", template)
+        ret = os.iorun(string.format("xim %s --disable-info -y", template))
+        if ret and string.find(ret, "installed") then
+            cprint("[xlings]: template [ ${green}%s${clear} ] installed", template)
+        else
+            cprint("[xlings]: ${red bright}failed to install template [%s]", template)
+            return nil
+        end
+    end
+
+    local info = xvm.info(pkgname, version or "")
+    if info["SPath"] then
+        return path.directory(info["SPath"])
+    end
+
+    return nil
+end
+
 function main(target, template)
 
     if target == nil or target == "" then
@@ -41,22 +84,26 @@ $ ${cyan}xlings install xlings-project-templates
     local projectdir = path.join(config.rundir, target)
 
     if not os.isdir(templatedir) then
+        templatedir = get_template_dir(template)
+    end
+
+    if not os.isdir(templatedir) then
         cprint("[xlings]: ${red bright}template [%s] not found...", template)
+    end
+
+    if os.isfile(projectdir) or os.isdir(projectdir) then
+        cprint("[xlings]: ${yellow bright}project dir [%s] already exists, please remove it first", target)
     else
-        if os.isfile(projectdir) or os.isdir(projectdir) then
-            cprint("[xlings]: ${yellow bright}project dir [%s] already exists, please remove it first", target)
-        else
-            cprint("[xlings]: create project...")
-            if os.trycp(templatedir, projectdir) then
-                os.cd(projectdir)
-                if os.isfile("config.xlings") then
-                    cprint("[xlings]: install project dependencies...")
-                    os.exec("xim")--os.exec("xim -y")
-                end
-                cprint("[xligns:d2x]: ${green}%s${clear} | ${yellow}%s${clear}", target, projectdir)
-            else
-                cprint("[xlings]: ${red bright}failed to create project${clear} from template [%s]", template)
+        cprint("[xlings]: create project...")
+        if os.trycp(templatedir, projectdir) then
+            os.cd(projectdir)
+            if os.isfile("config.xlings") then
+                cprint("[xlings]: install project dependencies...")
+                os.exec("xim")--os.exec("xim -y")
             end
+            cprint("[xligns:d2x]: ${green}%s${clear} | ${yellow}%s${clear}", target, projectdir)
+        else
+            cprint("[xlings]: ${red bright}failed to create project${clear} from template [%s]", template)
         end
     end
 end
