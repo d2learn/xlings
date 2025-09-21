1226c5c80fbc1407da5e06074f98d48f24112e20
sunrisepeak <speakshen@163.com>
2025-09-21 17:35:54 +0800

    core/xim: add types(template/script) features for xpkg and optimize project template
    1. add pm.types module - template
      - xvm program name: namespace-x-pkgname or template-x-pkgname
    2. change xscript default implement to types
    3. optimize 'xlings new', add --subpath for multi-template project
    4. optimize deps install to support version
    
    Signed-off-by: sunrisepeak <speakshen@163.com>

---
+++ core/xim/pm/types/template.lua
@@ -0,0 +1,51 @@
+import("xim.base.xvm")
+import("xim.base.runtime")
+
+function installed(xpkg)
+    local old_value = xvm.log_tag(false)
+    ret = xvm.has(__get_xvm_pkgname(xpkg), xpkg.version)
+    xvm.log_tag(old_value)
+    return ret
+end
+
+function install(xpkg)
+    local pkginfo = runtime.get_pkginfo()
+    local install_file = pkginfo.install_file
+
+    pkgdir = install_file
+        :replace(".zip", "")
+        :replace(".tar.gz", "")
+        :replace(".git", "")
+
+    os.tryrm(pkginfo.install_dir)
+
+    if not os.trymv(pkgdir, pkginfo.install_dir) then
+        cprint("[xlings:xim:template]: ${red bright}failed to install to %s${clear}", pkginfo.install_dir)
+        return false
+    end
+
+    xvm.add(__get_xvm_pkgname(xpkg))
+
+    return true
+end
+
+function uninstall(xpkg)
+    xvm.remove(__get_xvm_pkgname(xpkg), xpkg.version)
+    return true
+end
+
+--private
+
+function __get_xvm_pkgname(xpkg)
+    local pkgname = xpkg.name
+    local namespace = xpkg.namespace
+    local xvm_pkgname = ""
+
+    if namespace and namespace ~= "" then
+        xvm_pkgname = namespace .. "-x-" .. pkgname
+    else
+        xvm_pkgname = "template-x-" .. pkgname
+    end
+
+    return xvm_pkgname
+end
